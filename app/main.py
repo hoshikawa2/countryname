5a8e1e42b0b51634f34058586db527f291a61a0f
Cristiano Hoshikawa <hoshikawa@uol.com.br>
2024-05-26 20:59:09 -0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+
+from py_zipkin import Encoding #import Zipkin package
+from py_zipkin.zipkin import zipkin_span #import Zipkin package
+from transport import http_transport #import Zipkin transport
+import requests
+
+app = Flask(__name__)
+
+@zipkin_span(service_name='findCustomer', span_name='findCustomer')
+@app.route("/findCustomer/<string:nome>/<string:cpf>/<string:cartao>", methods=['GET'])
+def findCustomer(nome, cpf, cartao):
+    with zipkin_span(
+            service_name="findCustomer", #You can change it as you need
+            span_name="get", #You can change it as you need
+            transport_handler=http_transport, #zipkin transport, will use it to upload trace data to OCI APM
+            encoding = Encoding.V2_JSON,
+            binary_annotations = {"get":"customer information for BOT"}, #Custom tag
+            sample_rate=100 # this is optional and can be used to set custom sample rates
+    ):
+        nomeCompleto = "Cristiano Hoshikawa";
+        plano = "Plano Super";
+        return jsonify({'nomeCompleto': nomeCompleto, 'plano': plano});
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8082)
---
+++ app/rest.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import requests
+app = Flask(__name__)
+@app.route('/enviar_dados', methods=['POST'])
+def enviar_dados():    
+    body = request.data
+
+    #TRATAMENTO DO XML PASSADO DE REST PARA SOAP NO OIC
+    print(body)
+    body_string = body.decode('utf-8')
+    body_string = body_string.replace("[", "<")
+    body_string = body_string.replace("]", ">")
+    body_string = body_string.replace("<root>", '')
+    body_string = body_string.replace("</root>", '')
+    body_string = body_string.replace("\n", "")
+    body_string = body_string.replace("\t", "")
+    body = body_string.encode('utf-8')
+    print(body)
+
+    #CHAMADA SOAP PADRAO
+    headers = {'Content-Type': 'text/xml'}
+    url = 'http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?op=CountryName'
+    response = requests.post(url, data=body, headers=headers)
+    print(response.text)
+
+    #RETORNO REST COM XML
+    return response.text, 200, {'Content-Type': 'application/xml'}
+
+if __name__ == '__main__':
+    app.run()
